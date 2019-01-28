99d7d8336f81b697707523bb7a1914ba29724e97
Drew Cappel <drewcappel@drews-macbook-pro.local>
2019-01-28 12:16:47 -0500

    2.17 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
    
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var awesomeImageView: UIImageView!
+    
+    var index = 0
+    var imageIndex = -1
+    let numberOfImages = 6
+    var awesomePlayer = AVAudioPlayer()
+    let numberOfSounds = 5
+    var soundIndex = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    var index = 0
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
         
@@ -31,9 +38,9 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app!"]
         
-//        var newIndex = -1
         var newIndex: Int   // declares but doesn't intialize
         
+        // showing messages
         repeat {
             newIndex = Int.random(in:0..<messages.count)
         } while index == newIndex
@@ -41,35 +48,35 @@ class ViewController: UIViewController {
         index = newIndex
         messageLabel.text = messages[index]
         
+        // showing images
+        repeat {
+            newIndex = Int.random(in:0..<numberOfImages)
+        } while imageIndex == newIndex
         
-        // the func showMessagePressed() acts as the loop
-        // executed each time pressed and increments the index
+        imageIndex = newIndex
+        awesomeImageView.image = UIImage(named: "IMG_\(imageIndex)")
         
-//        messageLabel.text = messages.randomElement()!
+        // get a random number for sounds
+        repeat {
+            newIndex = Int.random(in:0..<numberOfSounds)
+        } while soundIndex == newIndex
         
-        // !!!!! when you know there is a value that can be chosen
-        // "force unwrap"
+        soundIndex = newIndex
         
-//        messageLabel.text = messages[index]
-//
-//        if index == messages.count - 1 {
-//            index = 0
-//        } else {
-//            index += 1
-//        }
-        
-//        let message1 = "You Are Awesome!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//        
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        } else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        } else {
-//            messageLabel.text = message1
-//        }
+        // playing sounds
+        var soundName = "sound\(soundIndex)"
+        // attempt to load in sound name
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+                } catch {
+                    print("Error: \(soundName) not found.")
+                }
+        } else {
+            // if load-in did not work
+            print("Error: \(soundName) not found.")
+        }
     }
-
 }
 
